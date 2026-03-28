908a6ee8ed5fa9aad2159c13484d986ecb428244
Arkadiusz Dzięcielewski <azden789@gmail.com>
2026-03-28 23:33:18 +0100

    Upgrade map markers with custom car pins and animations

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,22 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+const createCustomIcon = (isPromoted: boolean) => {
+  return L.divIcon({
+    className: 'custom-div-icon',
+    html: `
+      <div class="marker-pin ${isPromoted ? 'promoted' : ''}">
+        <div class="marker-icon-inner">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2"/><circle cx="7" cy="17" r="2"/><path d="M9 17h6"/><circle cx="17" cy="17" r="2"/></svg>
+        </div>
+      </div>
+    `,
+    iconSize: [36, 36],
+    iconAnchor: [18, 36],
+    popupAnchor: [0, -36]
+  });
+};
+
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -102,6 +118,7 @@ function App() {
                 <Marker 
                   key={wash.id} 
                   position={[wash.lat, wash.lng]}
+                  icon={createCustomIcon(!!wash.isPromoted)}
                   eventHandlers={{
                     click: () => handleMarkerClick(wash),
                   }}
